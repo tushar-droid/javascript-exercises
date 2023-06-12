80c30ddfa2fa62370ae208b8c2f7e7c75c8c697c
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-11 18:59:40 -0700

    Completed the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(st, num) {
+    let temp= '';
+    if (num<0){
+        return "ERROR";
+    }
+    for (let i= 0; i<num; i++){
+        temp= temp+st;
+    }
+    return temp;
 };
 
 // Do not edit below this line
