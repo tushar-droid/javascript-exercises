c6bcfe40d689f22f9716adadd64ace738b918732
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-11 20:21:17 -0700

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum= 0;
+    if(!(Number.isInteger(a) && Number.isInteger(b))){
+        return 'ERROR';
+    }
 
+
+    if (a<0 || b<0 ){
+        return 'ERROR';
+    }
+    if(a<b){
+        start= a;
+         end=b;
+    }
+
+    else{
+        start=b;
+        end=a;
+    }
+
+    for (let i= start; i<=end;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
