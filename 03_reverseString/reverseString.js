a717513d114dd017a9b0ae500e4d04eb564709b4
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-11 19:47:41 -0700

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let len= str.length;
+    let str_bld= '';
+    for (let i= len-1; i>=0;i--){
+        str_bld += str[i];
+    }
+    return str_bld;
 
 };
 
