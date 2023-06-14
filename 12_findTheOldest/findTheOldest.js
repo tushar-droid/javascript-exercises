d6e9f1d4ab19dce93c44238e9ed737b07e8031c8
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-14 00:17:20 -0700

    12 exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(lst) {
+    const sorted= lst.sort((a,b)=>{
+        const currDate= (new Date()).getFullYear(); 
+        let prevperson;
+        let nextperson;
+        if(isNaN(a.yearOfDeath)){
+            prevperson= currDate-a.yearOfBirth;
 
+        }
+        else{
+            prevperson= a.yearOfDeath-a.yearOfBirth;
+
+        }
+        if(isNaN(b.yearOfDeath)){
+            nextperson= currDate-b.yearOfBirth;
+
+        }
+        else{
+            nextperson= b.yearOfDeath-b.yearOfBirth;
+
+        }
+        if(prevperson>nextperson){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    })
+    return sorted[0];
 };
 
 // Do not edit below this line
