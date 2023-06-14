0972695de313170cf871be1aa27e867f828a6056
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-13 23:35:38 -0700

    exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str= str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    str= str.toLowerCase();
+    let j= str.length-1;
+    for( let i=0;i<str.length;i++){
+        if(str[i]!=str[j]){
+            return false;
+        }
+        j--;
+    }
+    return true;
+
 
 };
 
